32b231fda08d37c498c86caf1b9fffa3f68556b7
Leo5661 <singhra66@gmail.com>
2019-05-28 03:10:13 +0530

    updated development with master

--- app/src/main/java/com/coolopool/coolopool/Activity/SignInActivity.java
+++ app/src/main/java/com/coolopool/coolopool/Activity/SignInActivity.java
@@ -3,22 +3,41 @@ package com.coolopool.coolopool.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.coolopool.coolopool.R;
 
 public class SignInActivity extends AppCompatActivity {
+
+    Button mCreateButton;
+    TextView mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        Button mCreateButton = findViewById(R.id.createAccountButton);
+
+        // use for creating account
+        mCreateButton = findViewById(R.id.createAccountButton);
         mCreateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d("Log_test","Create Account has clicked");
+                Intent intent = new Intent(SignInActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Use if there is a account
+        mLoginButton = findViewById(R.id.Login_btn);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Log_test","I have account has Login clicked");
                 Intent intent = new Intent(SignInActivity.this,LoginActivity.class);
                 startActivity(intent);
             }
