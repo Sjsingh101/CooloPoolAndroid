6e05139c38507eb9d5d9bac320c6a3bfa803521e
DANISH <developer.danish.jamal@gmail.com>
2019-05-29 11:09:02 +0530

    Implementation of post chooser

--- app/src/main/java/com/coolopool/coolopool/Activity/HomeActivity.java
+++ app/src/main/java/com/coolopool/coolopool/Activity/HomeActivity.java
@@ -2,6 +2,8 @@ package com.coolopool.coolopool.Activity;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.design.bottomappbar.BottomAppBar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +15,7 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.coolopool.coolopool.Adapter.PostAdapter;
 import com.coolopool.coolopool.R;
@@ -54,15 +57,43 @@ public class HomeActivity extends AppCompatActivity {
         addPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(HomeActivity.this,NewPostActivity.class));
+                showDialog();
             }
         });
 
 
     }
 
-    private void showDialoge(){
+    private void showDialog(){
         Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.post_method_chooser);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        ((TextView)dialog.findViewById(R.id.new_post_pic)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Todo: create another activity for user to create their trip of only pictures
+
+            }
+        });
+        ((TextView)dialog.findViewById(R.id.new_post_blog)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Todo: create another activity for user to create their trip in form of blog(pic plus caption)
+
+            }
+        });
+        ((TextView)dialog.findViewById(R.id.new_post_trip)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Normal way of creating post using trip details
+                startActivity(new Intent(HomeActivity.this,NewPostActivity.class));
+            }
+        });
+
+        dialog.setCancelable(true);
+
+        dialog.show();
 
     }
 
