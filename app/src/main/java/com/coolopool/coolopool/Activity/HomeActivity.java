0b8830f245c833aa0db2808ed8252e783c2e7999
Leo5661 <singhra66@gmail.com>
2019-05-30 02:46:14 +0530

    updated with master

--- app/src/main/java/com/coolopool/coolopool/Activity/HomeActivity.java
+++ app/src/main/java/com/coolopool/coolopool/Activity/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.coolopool.coolopool.Activity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.design.bottomappbar.BottomAppBar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +15,7 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.coolopool.coolopool.Adapter.PostAdapter;
 import com.coolopool.coolopool.R;
@@ -53,9 +57,44 @@ public class HomeActivity extends AppCompatActivity {
         addPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                showDialog();
+            }
+        });
+
+
+    }
+
+    private void showDialog(){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.post_method_chooser);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        ((TextView)dialog.findViewById(R.id.new_post_pic)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Todo: create another activity for user to create their trip of only pictures
+
+            }
+        });
+        ((TextView)dialog.findViewById(R.id.new_post_blog)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Todo: create another activity for user to create their trip in form of blog(pic plus caption)
+
+            }
+        });
+        ((TextView)dialog.findViewById(R.id.new_post_trip)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Normal way of creating post using trip details
                 startActivity(new Intent(HomeActivity.this,NewPostActivity.class));
             }
         });
+
+        dialog.setCancelable(true);
+
+        dialog.show();
+
     }
 
 }
