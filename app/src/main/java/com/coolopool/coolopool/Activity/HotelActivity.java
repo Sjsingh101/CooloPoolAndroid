3e7cd2c16d4ea74f36c14500849952a9e02aa6ad
DANISH <developer.danish.jamal@gmail.com>
2019-06-12 11:43:59 +0530

    Adapter and class for Hotel List Item added

--- app/src/main/java/com/coolopool/coolopool/Activity/HotelActivity.java
+++ app/src/main/java/com/coolopool/coolopool/Activity/HotelActivity.java
@@ -3,43 +3,54 @@ package com.coolopool.coolopool.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.coolopool.coolopool.Adapter.HotelAdapter;
+import com.coolopool.coolopool.Class.Hotel;
 import com.coolopool.coolopool.Helper.DialogBuilder;
 import com.coolopool.coolopool.R;
 
+import java.util.ArrayList;
+
 public class HotelActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+
+    HotelAdapter mHotelAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotel);
 
-        Intent intent = getIntent();
+        ArrayList<Hotel> hotels = new ArrayList<>();
 
-        String location = intent.getStringExtra("LOCATION");
-        String guests = intent.getStringExtra("GUESTS");
-        String rooms = intent.getStringExtra("ROOMS");
-        String checkInDate = intent.getStringExtra("CHECKIN_DAYOFWEEK").substring(0, 3)+ ", " + intent.getStringExtra("CHECKIN") + intent.getStringExtra("CHECKIN_MONTH_YEAR");
-        String checkOutDate = intent.getStringExtra("CHECKOUT_DAYOFWEEK").substring(0, 3)+ ", " + intent.getStringExtra("CHECKOUT") + intent.getStringExtra("CHECKOUT_MONTH_YEAR");
-
-        TextView location_textView = (TextView)findViewById(R.id.hotel_activity_location_textView);
-        TextView checkIn_textView = (TextView)findViewById(R.id.hotel_activity_date_from_dayOfWeek_textView);
-        TextView checkOut_textView = (TextView)findViewById(R.id.hotel_activity_date_to_dayOfWeek_textView);
-        TextView guests_textView = (TextView)findViewById(R.id.hotel_activity_guests_value_textView);
-        TextView rooms_textView = (TextView)findViewById(R.id.hotel_activity_rooms_value_textView);
-
-        location_textView.setText(location);
-        checkIn_textView.setText(checkInDate);
-        checkOut_textView.setText(checkOutDate);
-        guests_textView.setText(guests);
-        rooms_textView.setText(rooms);
+        hotels.add(new Hotel("ABCD", "4000", "Mall road", "url", (float)3.5));
+        hotels.add(new Hotel("Residence", "5500", "Mall road", "url", (float)5));
+        hotels.add(new Hotel("OYO 337", "6079", "GT road", "url", (float)4.5));
+        hotels.add(new Hotel("OYO 334", "4040", "Nh32 road", "url", (float)2.5));
+        hotels.add(new Hotel("OYO 654", "4400", "Nh21 road", "url", (float)4.5));
+        hotels.add(new Hotel("OYO 323", "8000", "Mall road", "url", (float)5));
+        hotels.add(new Hotel("OYO 110", "4060", "Mall road", "url", (float)4.5));
+        hotels.add(new Hotel("OYO 111", "9000", "Mall road", "url", (float)1.5));
+        hotels.add(new Hotel("OYO 999", "4560", "Mall road", "url", (float)4.5));
+        hotels.add(new Hotel("OYO 600", "4900", "Mall road", "url", (float)5));
+        hotels.add(new Hotel("OYO 33", "4670", "Mall road", "url", (float)4.25));
 
 
+        mRecyclerView = (RecyclerView)findViewById(R.id.hotel_activity_recyclerView);
+        mHotelAdapter = new HotelAdapter(hotels);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mHotelAdapter);
+
+        getContentsFromIntent();
 
         final ImageButton filter = (ImageButton)findViewById(R.id.hotel_activity_filter);
         ImageButton backButton = (ImageButton)findViewById(R.id.hotel_activity_back_button);
@@ -71,5 +82,27 @@ public class HotelActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    private void getContentsFromIntent(){
+        Intent intent = getIntent();
+
+        String location = intent.getStringExtra("LOCATION");
+        String guests = intent.getStringExtra("GUESTS");
+        String rooms = intent.getStringExtra("ROOMS");
+        String checkInDate = intent.getStringExtra("CHECKIN_DAYOFWEEK").substring(0, 3)+ ", " + intent.getStringExtra("CHECKIN") + intent.getStringExtra("CHECKIN_MONTH_YEAR");
+        String checkOutDate = intent.getStringExtra("CHECKOUT_DAYOFWEEK").substring(0, 3)+ ", " + intent.getStringExtra("CHECKOUT") + intent.getStringExtra("CHECKOUT_MONTH_YEAR");
+
+        TextView location_textView = (TextView)findViewById(R.id.hotel_activity_location_textView);
+        TextView checkIn_textView = (TextView)findViewById(R.id.hotel_activity_date_from_dayOfWeek_textView);
+        TextView checkOut_textView = (TextView)findViewById(R.id.hotel_activity_date_to_dayOfWeek_textView);
+        TextView guests_textView = (TextView)findViewById(R.id.hotel_activity_guests_value_textView);
+        TextView rooms_textView = (TextView)findViewById(R.id.hotel_activity_rooms_value_textView);
+
+        location_textView.setText(location);
+        checkIn_textView.setText(checkInDate);
+        checkOut_textView.setText(checkOutDate);
+        guests_textView.setText(guests);
+        rooms_textView.setText(rooms);
     }
 }
---
+++ app/src/main/java/com/coolopool/coolopool/Adapter/HotelAdapter.java
@@ -0,0 +1,75 @@
+package com.coolopool.coolopool.Adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v4.media.RatingCompat;
+import android.support.v7.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.coolopool.coolopool.Class.Hotel;
+import com.coolopool.coolopool.R;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.HotelViewHolder> {
+
+    ArrayList<Hotel> hotelList;
+
+    public HotelAdapter(ArrayList<Hotel> hotelAList) {
+        this.hotelList = hotelAList;
+    }
+
+    @NonNull
+    @Override
+    public HotelViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.hotel_list_item, viewGroup, false);
+        HotelViewHolder hotelViewHolder = new HotelViewHolder(view);
+        return hotelViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HotelViewHolder hotelViewHolder, int i) {
+        Hotel currentHotel = hotelList.get(i);
+
+        hotelViewHolder.name.setText(currentHotel.getmName());
+        hotelViewHolder.cost.setText(currentHotel.getmCost());
+        hotelViewHolder.location.setText(currentHotel.getmLocation());
+
+        hotelViewHolder.rating.setRating((float)currentHotel.getmRating());
+
+        Picasso.get().load(currentHotel.getmThumbnailUrl()).error(R.drawable.pic1).into(hotelViewHolder.thumbnail);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return hotelList.size();
+    }
+
+    public static class HotelViewHolder extends RecyclerView.ViewHolder{
+
+        TextView name;
+        TextView cost;
+        TextView location;
+        RatingBar rating;
+        ImageView thumbnail;
+
+        public HotelViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = (TextView)itemView.findViewById(R.id.hotel_list_item_hotel_name_textView);
+            cost = (TextView)itemView.findViewById(R.id.hotel_list_item_cost_textView);
+            location = (TextView)itemView.findViewById(R.id.hotel_list_item_location_textView);
+            rating = (RatingBar)itemView.findViewById(R.id.hotel_list_item_ratingBar);
+            thumbnail = (ImageView)itemView.findViewById(R.id.hotel_list_item_imageView);
+        }
+    }
+}
