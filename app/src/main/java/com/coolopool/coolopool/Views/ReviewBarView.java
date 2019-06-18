47a65419e818f70449d5d207d7b9bbabfab57d31
DANISH <developer.danish.jamal@gmail.com>
2019-06-18 19:01:58 +0530

    ReviewBarView class added

---
+++ app/src/main/java/com/coolopool/coolopool/Views/ReviewBarView.java
@@ -0,0 +1,19 @@
+package com.coolopool.coolopool.Views;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class ReviewBarView extends View {
+
+
+    public ReviewBarView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    private void init(AttributeSet set){
+        
+    }
+}
