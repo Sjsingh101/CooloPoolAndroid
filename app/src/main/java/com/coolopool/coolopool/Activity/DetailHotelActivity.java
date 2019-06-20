0c05bdfd5a3c159a19e4396bce47958ac800d745
DANISH <developer.danish.jamal@gmail.com>
2019-06-20 15:55:33 +0530

    Final commit of this branch: All UI is done, with some minor bugs like (heart button animation), (Color of text in app bar layout), (Minor fixes)

--- app/src/main/java/com/coolopool/coolopool/Activity/DetailHotelActivity.java
+++ app/src/main/java/com/coolopool/coolopool/Activity/DetailHotelActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
