ead0f1dfd9059a7ed8e8a4ea1412e004a5c16176
DANISH <developer.danish.jamal@gmail.com>
2019-06-23 12:30:24 +0530

    RestaurantAdapter: layout inflated

--- app/src/main/java/com/coolopool/coolopool/Adapter/RestaurantAdapter.java
+++ app/src/main/java/com/coolopool/coolopool/Adapter/RestaurantAdapter.java
@@ -2,29 +2,36 @@ package com.coolopool.coolopool.Adapter;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.coolopool.coolopool.Class.Restaurant;
 import com.coolopool.coolopool.R;
 
+import java.util.ArrayList;
+
 public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.RestaurantViewHolder> {
+
+    ArrayList<Restaurant> restaurants;
 
     @NonNull
     @Override
     public RestaurantViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+         View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.restaurant_list_item, viewGroup, false);
+        return new RestaurantViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RestaurantViewHolder restaurantViewHolder, int i) {
-
+        Restaurant currentRestaurant = restaurants.get(i);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return restaurants.size();
     }
 
     public class RestaurantViewHolder extends RecyclerView.ViewHolder{
