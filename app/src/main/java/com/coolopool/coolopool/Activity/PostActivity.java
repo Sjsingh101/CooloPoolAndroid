cc5854bef93fc5d34449de245ecd69868e94d920
DANISH <developer.danish.jamal@gmail.com>
2019-07-02 01:13:46 +0530

    activity_post.xml: ui implemented 50%

--- app/src/main/java/com/coolopool/coolopool/Activity/PostActivity.java
+++ app/src/main/java/com/coolopool/coolopool/Activity/PostActivity.java
@@ -2,12 +2,16 @@ package com.coolopool.coolopool.Activity;
 
 import android.os.Build;
 import android.provider.ContactsContract;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 
 import com.coolopool.coolopool.R;
 
@@ -21,7 +25,7 @@ public class PostActivity extends AppCompatActivity {
 
 
 
-        ImageView back = (ImageView)findViewById(R.id.post_activity_back_button);
+        final ImageView back = (ImageView)findViewById(R.id.post_activity_back_button);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,6 +33,19 @@ public class PostActivity extends AppCompatActivity {
             }
         });
 
+        final ScrollView scrollView = (ScrollView)findViewById(R.id.post_activity_scrollView);
+        final NestedScrollView root = (NestedScrollView)findViewById(R.id.nested);
+
+        scrollView.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+            @Override
+            public void onScrollChanged() {
+                int[] loc = new int[2];
+                back.getLocationOnScreen(loc);
+                Log.d("~~~~~~~~~~~~~~~",":----------- "+loc[0]);
+
+
+            }
+        });
 
 
     }
