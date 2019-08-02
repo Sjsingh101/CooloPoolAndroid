fa6fc59f289b7e9d90a1d322d401935fd9e7800c
DANISH <developer.danish.jamal@gmail.com>
2019-08-02 19:51:34 +0530

    wallet ui updated

--- app/src/main/java/com/coolopool/coolopool/Activity/WalletActivity.java
+++ app/src/main/java/com/coolopool/coolopool/Activity/WalletActivity.java
@@ -11,5 +11,8 @@ public class WalletActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wallet);
+
+        
+
     }
 }
